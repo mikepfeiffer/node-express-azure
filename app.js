f13eed91cc579607ce355dacc087f39c89fe5b1d
mikepfeiffer <mike.pfeiffer@gmail.com>
2018-12-21 14:39:32 -0700

    updated tests

--- app.js
+++ app.js
@@ -3,31 +3,21 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+const index = require('./routes/index');
+const who = require('./routes/who');
+const contact = require('./routes/contact');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('port', config.port);
+
 app.use('/', express.static('public'))
-
-app.get('/', (req, res) => {
-  res.render('home', {
-    title: 'Welcome to Contoso!',   
-  });
-});
-
-app.get('/who', (req, res) => {
-  res.render('who', {
-    title: 'Who We Are',   
-  });
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact', {
-    title: 'Contact Us',   
-  });
-});
+app.use('/', index);
+app.use('/who', who);
+app.use('/contact', contact);
 
 app.listen(config.port, () => {
   console.log(`Demo app is running on ${config.port}!`);
