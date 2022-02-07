92743856b5f44f3c30db4107b6b86f318d12ce7a
Alok Shrivastava <alok.shrivastava@qritive.com>
2022-02-08 03:42:31 +0530

    changed title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Welcome to Qritive-Healthcare-Product!',   
+    title: 'Welcome to Qritive-Healthcare-Singapore!',   
   });
 });
 
