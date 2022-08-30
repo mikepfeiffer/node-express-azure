c8f170f1e676758299a718e6ad4d0d1bc482fe37
ClongA360 <100709640+ClongA360@users.noreply.github.com>
2022-08-29 20:27:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const contact = require('./routes/contact');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('port', config.port);
 
