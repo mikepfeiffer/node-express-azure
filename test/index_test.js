850597d0dc114eb91f456a1e7e76b139f9ad9370
barnwal-paras <parasbarnwal06@gmail.com>
2022-10-23 16:50:25 +0530

    Update index_test.js

--- test/index_test.js
+++ test/index_test.js
@@ -8,13 +8,14 @@ const server = require('../app');
 chai.use(chaiHttp);
 
 describe('/GET', () => {
-    it('returns the homepage', (done) => {
-        chai.request(`http://localhost:${config.port}`)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.text.should.contain('Welcome to Contoso!');
-                done();
-            });
-    });
+  it('returns the homepage', (done) => {
+    chai
+      .request(`http://localhost:${config.port}`)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.text.should.contain('Contoso is shit');
+        done();
+      });
+  });
 });
